3fb4af93dda0bfc2c923401975e3f0e694db6ef4
Lee YongSeong <151592314+EDragonCastle@users.noreply.github.com>
2025-07-07 10:36:16 +0900

    Create Unity Initalize Folder
    Unity에 필요한 초기 파일인 Material, Model, Resource, Script, Shader, Texture 파일을 생성했습니다. 그 안에 간단한 예시 Script와 Shader Material을 생성했습니다.

---
+++ Empty/Assets/Script/Initalize Test Script.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class InitalizeTestScript : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello Unity World");
+    }
+
+    void Update()
+    {
+        
+    }
+}
