5ce124cb0be7c35efae5e246ecef132f626d424d
Lee YongSeong <151592314+EDragonCastle@users.noreply.github.com>
2025-07-18 12:47:27 +0900

    쓸모 없는 Commnet 삭제

--- Empty/Assets/Script/Element.cs
+++ Empty/Assets/Script/Element.cs
@@ -2,16 +2,10 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using System;
 
-// RectTransform을 반드시 넣어야 하는데 요구하는 거 넣어도 되지 않나?
 [RequireComponent(typeof(RectTransform))]
 public class Element : MonoBehaviour, IPointerDownHandler
 {
-    // Color랑 Position이랑 isVisits는 그냥 적용해도 괜찮은데
-    // Rect Transform은 바로 적용해야할 것 같다.
     private RectTransform uiPosition;
-
-    // 이걸 이용할 것이다.
-    [SerializeField]
     private ElementInfo elementInfo;
 
     #region Property RectTransform
@@ -23,7 +17,6 @@ public class Element : MonoBehaviour, IPointerDownHandler
     public ElementInfo GetElementInfo() => elementInfo;
     public void SetElementInfo(ElementInfo info) => elementInfo = info;
     #endregion
-
 
     private void Awake()
     {
